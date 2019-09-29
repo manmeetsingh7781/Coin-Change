6512be058a34a0847765689b3695940c8d52f9c7
Manmeet Singh <35901948+manmeetsingh7781@users.noreply.github.com>
2019-09-28 20:54:36 -0700

    Create CoinChange.py

---
+++ CoinChange.py
@@ -0,0 +1,34 @@
+def get_coin_type(coin_type):
+    if coin_type == 10:
+        coin_type = 'Dime'
+    elif coin_type == 25:
+        coin_type = 'Quarter'
+    elif coin_type == 5:
+        coin_type = 'Nickle'
+    elif coin_type == 1:
+        coin_type = "Penni"
+    return coin_type
+
+
+def getChange(cents):
+    drawer = [25, 10, 5, 1]
+    filtered_drawer = set()
+    remainder = coin_type = 0
+    min_amount = 25
+    for i in drawer:
+        if i <= cents:
+            filtered_drawer.add(i)
+
+    for i in sorted(filtered_drawer):
+        result = cents // i
+        remainder = cents % i
+        if result < min_amount:
+            coin_type = i
+            min_amount = result
+    if remainder != 0:
+        print(getChange(remainder))
+    coin = get_coin_type(coin_type)
+    return f'{min_amount} of {coin}s'
+
+
+print(getChange(67))
